e6f5adc89b9b5119afb570796e19ea932776bde5
Mient-jan Stelling <mientjan.stelling@gmail.com>
2018-10-02 17:48:30 +0200

    adding fromUrlAndElement

--- index.js
+++ index.js
@@ -2,12 +2,52 @@ const puppeteer = require("puppeteer");
 const httpServer = require("http-server/lib/http-server");
 const portfinder = require("portfinder");
 
-async function fromUrl({ url, config, location }) {
+/**
+ *
+ * @param {string} url
+ * @param {any} config
+ * @param {string} location
+ * @param {object} clip
+ * @param {number} clip.x
+ * @param {number} clip.y
+ * @param {number} clip.width
+ * @param {number} clip.height
+ * @return {Promise<void>}
+ */
+async function fromUrl({ url, config, location, clip = null }) {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto(url);
   await page.screenshot({ path: location });
   await browser.close();
+}
+
+
+async function fromUrlAndElement({ selector, url, config, location }) {
+	const padding = 0;
+	const browser = await puppeteer.launch();
+	const page = await browser.newPage();
+
+	await page.goto(url);
+
+	const rect = await page.evaluate(selector => {
+		const element = document.querySelector(selector);
+		const { x, y, width, height } = element.getBoundingClientRect();
+		return { left: x, top: y, width, height, id: element.id };
+	}, selector);
+
+	const settings = {
+		path: location,
+		clip: {
+			x: rect.left - padding,
+			y: rect.top - padding,
+			width: rect.width + padding * 2,
+			height: rect.height + padding * 2
+		}
+	};
+
+	await page.screenshot(settings);
+	await browser.close();
 }
 
 /**
