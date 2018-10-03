8edca8762f2211eec26b01edcd1f03de375862b6
Mient-jan Stelling <mientjan.stelling@gmail.com>
2018-10-03 14:31:52 +0200

    refactoring fromUrl removing fromUrlAndElement integrating it into fromUrl

--- index.js
+++ index.js
@@ -7,47 +7,53 @@ const portfinder = require("portfinder");
  * @param {string} url
  * @param {any} config
  * @param {string} location
- * @param {object} clip
- * @param {number} clip.x
- * @param {number} clip.y
- * @param {number} clip.width
- * @param {number} clip.height
+ * @param {string} selector
+ * @param {string} waitUntil
  * @return {Promise<void>}
  */
-async function fromUrl({ url, config, location, clip = null }) {
+async function fromUrl({
+  url,
+  config = {},
+  location,
+  selector = null,
+  waitUntil = "load"
+}) {
+  const padding = 0;
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto(url);
-  await page.screenshot({ path: location });
+
+  await page.goto(url, { waitUntil });
+  let rect = null;
+
+  if (selector !== null) {
+    rect = await page.evaluate(selector => {
+      const element = document.querySelector(selector);
+
+      if (!element) {
+        const { innerWidth: width, innerHeight: height } = window;
+        return { left: 0, top: 0, width, height };
+      }
+
+      const { x, y, width, height } = element.getBoundingClientRect();
+      return { left: x, top: y, width, height };
+    }, selector);
+  }
+
+  const settings = {
+    path: location
+  };
+
+  if (rect) {
+    settings.rect = {
+      x: rect.left - padding,
+      y: rect.top - padding,
+      width: rect.width + padding * 2,
+      height: rect.height + padding * 2
+    };
+  }
+
+  await page.screenshot(settings);
   await browser.close();
-}
-
-
-async function fromUrlAndElement({ selector, url, config, location }) {
-	const padding = 0;
-	const browser = await puppeteer.launch();
-	const page = await browser.newPage();
-
-	await page.goto(url);
-
-	const rect = await page.evaluate(selector => {
-		const element = document.querySelector(selector);
-		const { x, y, width, height } = element.getBoundingClientRect();
-		return { left: x, top: y, width, height, id: element.id };
-	}, selector);
-
-	const settings = {
-		path: location,
-		clip: {
-			x: rect.left - padding,
-			y: rect.top - padding,
-			width: rect.width + padding * 2,
-			height: rect.height + padding * 2
-		}
-	};
-
-	await page.screenshot(settings);
-	await browser.close();
 }
 
 /**
@@ -124,4 +130,3 @@ async function fromElement({ selector, filepath, config, location }) {
 module.exports.fromUrl = fromUrl;
 module.exports.fromPath = fromPath;
 module.exports.fromElement = fromElement;
-module.exports.fromUrlAndElement = fromUrlAndElement;
